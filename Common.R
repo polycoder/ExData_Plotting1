c2e651c9de536185fee6f69cb61794288c5bbc28
Sean Kenny <seank@spotify.com>
2016-01-10 00:18:11 -0500

    Adding zip download/extract logic.

--- Common.R
+++ Common.R
@@ -1,15 +1,26 @@
-# load polycoder_pwr_data table if it does not exist in the environment.
+# load polycoder_pwr_data table if it does not exist in the environment already. Helpul since it takes a long time to read it in, so skip if possible...
 if(!exists("polycoder_pwr_data")) {
   script.dir <- dirname(sys.frame(1)$ofile)
+
+  data_file <- paste(script.dir, "household_power_consumption.txt", sep="/")
+
+  # if we don't have a data file then download and unzip it now.
+  if(!file.exists(data_file)){
+    zip_file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    zip_file <- paste(script.dir, "exdata-data-household_power_consumption.zip", sep="/")
+    download.file(zip_file_url, zip_file, mode="wb", method="curl")
+    unzip(zip_file, exdir=script.dir)
+  }
+
   polycoder_pwr_data <- read.csv(
-    paste(script.dir, "household_power_consumption.txt", sep="/"), 
+    data_file,
     sep=";",
     dec=".", 
     na.strings="?",
     header=TRUE,
     stringsAsFactors=FALSE
   )
-  
+
   # convert date and time information.
   polycoder_pwr_data <- polycoder_pwr_data[polycoder_pwr_data$Date=="1/2/2007"|polycoder_pwr_data$Date=="2/2/2007",]
   polycoder_pwr_data$Time <- strptime(paste(polycoder_pwr_data$Date, polycoder_pwr_data$Time), "%d/%m/%Y %H:%M:%S")
@@ -37,7 +48,7 @@ plot_global_active_power <- function(ylab="Global Active Power (kilowatts)") {
 plot_sub_metering <- function() {
   yrange<-range(c(polycoder_pwr_data$Sub_metering_1,polycoder_pwr_data$Sub_metering_2,polycoder_pwr_data$Sub_metering_3))
   yrange[[2]] <- yrange[[2]] + 5 # +5 for padding from legend
-  
+
   plot(polycoder_pwr_data$Time, polycoder_pwr_data$Sub_metering_1,
        ylim=yrange,
        type="o", 
@@ -48,7 +59,7 @@ plot_sub_metering <- function() {
   )
   lines(polycoder_pwr_data$Time, polycoder_pwr_data$Sub_metering_2,type="l",col="red")
   lines(polycoder_pwr_data$Time, polycoder_pwr_data$Sub_metering_3,type="l",col="blue")
-  
+
   legend("topright", 
          c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
          lwd=c(1,1,1),
